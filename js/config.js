534c03f44c2a4cd6b97bb9dddfb41f5e1377e5d2
klacy6 <klacy6@jh.edu>
2024-10-06 07:42:48 -0700

    updat config.js file

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-west-1_D1tustMru', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '37o8mt6ft02hv8tqe2o5l7o7j2', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'us-west-1' // e.g. us-east-2
+        userPoolId: 'us-west-1_atLBn7jHa',
+        userPoolClientId: '877dj25i4jtkjke6dsdacnu38', 
+        region: 'us-west-1' 
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: '',
     }
 };
